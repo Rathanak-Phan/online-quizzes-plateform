d1537873f0b8e727baf1a9930d26122a406d4085
Rathanak-Phan <studyingalaxy123@gmail.com>
2025-12-11 01:38:11 +0700

    Update

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const PORT = 3000;
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Welcome to my backend");
-})
 
-app.listen(PORT, () =>{
-    console.log(`Server is running at http://localhost:${PORT}`);
-    
-})
+
+app.get("/api", (req, res) => {
+  res.send("Welcome to my backend");
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
