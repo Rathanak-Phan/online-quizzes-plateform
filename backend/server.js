77df4677e1c2a61b9172da35072dbdf41bb1b359
Rathanak-Phan <studyingalaxy123@gmail.com>
2025-12-12 23:25:37 +0700

    New branch for dev

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/api", (req, res) => {
-  res.send("Welcome to my data from backend...");
+  res.send("Welcome to my data from backend...develop");
 });
 
 // Add your auth routes
